f82211533770696f472cba82f7dad62d8fd8e1ec
Giorgi <gmukhigulashvili17@gmail.com>
2016-02-04 12:53:26 +0400

    added drawer layout

--- app/src/main/java/com/eventsview/eventsview/MainActivity.java
+++ app/src/main/java/com/eventsview/eventsview/MainActivity.java
@@ -1,23 +1,27 @@
 package com.eventsview.eventsview;
 
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    
+    private static final int LAYOUT = R.layout.layout_main;
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_main);
+        setContentView(LAYOUT);
 
         initToolbar();
+        inittNavigationView();
     }
 
     private void initToolbar() {
@@ -30,5 +34,9 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void inittNavigationView() {
+        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
     }
 }
